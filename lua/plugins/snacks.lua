3ae756eadb759f9e345acb5d03741b9a754247e1
Bruno Pessoa <71235114+brunofpessoa@users.noreply.github.com>
2024-12-11 20:51:54 -0300

    add snacks.nvim

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+    "folke/snacks.nvim",
+    -- keys = {
+    --     { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+    --     { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" }
+    -- },
+    opts = {
+        indent = {
+            chunk = {
+                enabled = true,
+                char = {
+                    corner_top = "╭",
+                    corner_bottom = "╰",
+                    arrow = "",
+                },
+            }
+        },
+        scratch = {},
+        bigfile = {},
+    },
+    -- config = function ()
+    --
+    -- end
+}
