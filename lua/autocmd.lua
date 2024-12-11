cb83d15014c4537c4a8683ed9b4f2dc9f53e6c49
Bruno Pessoa <71235114+brunofpessoa@users.noreply.github.com>
2024-12-11 20:58:05 -0300

    fix autocmds

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -26,20 +26,31 @@ end
 vim.api.nvim_create_autocmd("DirChanged", {
     pattern = { "*" },
     group = augroup,
-    desc = "Teak things in obsidian vault",
+    desc = "Change colorscheme when in obsidian vault",
     callback = function()
         if vim.uv.cwd() == require('constants').OBSIDIAN_VAULT then
-            enable_incremental_selection(false)
             vim.schedule(function()
                 math.randomseed(os.time())
                 local idx = math.random(#colorschemes.light)
                 vim.cmd.colorscheme(colorschemes.random and colorschemes.light[idx] or colorschemes.default_markdown)
             end)
         else
-            enable_incremental_selection(true)
             vim.schedule(function()
                 vim.cmd.colorscheme(colorschemes.default_others)
             end)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "*" },
+    group = augroup,
+    desc = "Toggle incremental selection when in obsidian vault",
+    callback = function()
+        if vim.bo.filetype == "markdown" then
+            enable_incremental_selection(false)
+        else
+            enable_incremental_selection(true)
         end
     end,
 })
